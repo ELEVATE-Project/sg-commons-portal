53b6231c259ffc2e961fb7e5d4d1b49ecf76caaa
Syed Ameen <syedameenabrar786@gmail.com>
2026-07-02 11:27:41 +0530

    server path updated to /sg-comms

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = process.env.PORT || 1819;
 
-app.use('/mohini', express.static(path.join(__dirname, 'build')));
+app.use('/sg-commons', express.static(path.join(__dirname, 'build')));
 
-app.get('/mohini/*', (req, res) => {
-    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-    res.header('Expires', '-1');
-    res.header('Pragma', 'no-cache');
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.get('/sg-commons/*', (req, res) => {
+  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+  res.header('Expires', '-1');
+  res.header('Pragma', 'no-cache');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
